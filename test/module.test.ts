86ec7b0227bd3795111c1a3151c9f8fb096035d3
Charles Lowell <cowboyd@frontside.com>
2022-11-11 13:52:23 -0600

    refactor tests to use a non-silly import scheme

--- test/module.test.ts
+++ test/module.test.ts
@@ -1,34 +1,25 @@
+import type { YSModule } from "../types.ts";
+import type { Task } from "../deps.ts";
+
 import { describe, it, expect } from './suite.ts';
 import { load, ys2js, evaluate } from "../mod.ts";
 import { run } from "../deps.ts";
 
-const base = `${import.meta.url}`;
-
-const urls = {
-  one: new URL("modules/1.yaml", base),
-  empty: new URL("modules/empty.yaml", base),
-  nodeps: new URL("modules/nodeps.yaml", base),
-  importsRelative: new URL("modules/imports-relative.yaml", base),
-  importsAbsolute: new URL("modules/imports-absolute.yaml", base),
-  noSuchSymbol: new URL("modules/no-such-symbol.yaml", base),
-  ts: new URL("modules/module.yaml.ts", base),
-};
-
 describe("a YAMLSCript module", () => {
   it("can be loaded from an absolute url", async () => {
-    let mod = await run(() => load(urls.one));
+    let mod = await loadmod("1.yaml");
     expect(mod.symbols).toBeDefined();
     expect(mod.symbols.type).toEqual("map");
   });
   it("has a list of exported symbols", async () => {
-    let mod = await run(() => load(urls.nodeps));
+    let mod = await loadmod("nodeps.yaml");
     let symbols = mod.symbols;
 
     expect(ys2js(symbols.value.five)).toEqual(5);
     expect(symbols.value.id.type).toEqual("fn");
   });
   it("can load other modules from a relative url", async () => {
-    let mod = await run(() => load(urls.importsRelative));
+    let mod = await loadmod("imports-relative.yaml");
     expect(mod.symbols.value.five.type).toEqual("number");
 
     expect(ys2js(mod.symbols.value.five)).toEqual(5);
@@ -38,30 +29,30 @@ describe("a YAMLSCript module", () => {
   });
 
   it("does not import symbols that are not expliticly mapped", async () => {
-    let mod = await run(() => load(urls.importsRelative));
+    let mod = await loadmod("imports-relative.yaml");
     expect(mod.symbols.value.str).not.toBeDefined();
   });
 
   it("throws an error if the symbol does not exist in the source module", async ()=> {
     try {
-      await run(() => load(urls.noSuchSymbol));
+      await loadmod("no-such-symbol.yaml");
       throw new Error('importing a symbol that does not exist should fail');
     } catch (error) {
       expect(error.message).toMatch(/does not define/);
     }
   });
   it("can be specified using a TypeScript module", async () => {
-    let mod = await run(() => load(urls.ts));
+    let mod = await loadmod("module.yaml.ts")
     let result = await evaluate(`{to-string: 100 }`, { context: mod.symbols });
     expect(result).toEqual({ type: "string", value: "100" });
   });
 
-  // it("can be specified using a JavaScript module", async () => {
+  // it("can support a `do` expression at the module level", () => {
+  //   let mod = await loadmod("do.yaml");
 
   // });
-  // it("will be treated as the body of a 'do' expression if it is a bare list");
-  // it("can support a `do` expression at the module level");
 
+  // it("will be treated as the body of a 'do' expression if it is a bare list");
   // it("can remap names of imported symbols");
   // it("can import from multiple different modules");
   // it("can load other modules from an absolute url");
@@ -72,3 +63,7 @@ describe("a YAMLSCript module", () => {
   // it("can handle circular module references");
   // it("can be specified using WASM");
 })
+
+function loadmod(url: string): Task<YSModule> {
+  return run(() => load(new URL(`modules/${url}`, import.meta.url)));
+}
