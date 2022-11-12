6cb6be912a70bf291f320517c658f35fb8c39415
Charles Lowell <cowboyd@frontside.com>
2022-11-12 16:16:42 -0600

    Add a log function, and something to capture its contents

--- ys.ts
+++ ys.ts
@@ -1,20 +1,38 @@
 import type { Operation } from "./deps.ts";
 import { signal } from "https://deno.land/std@0.159.0/signal/mod.ts";
-import { first, run, resolve, stream } from './deps.ts';
+import { first, run, resolve, spawn, stream, LogContext } from './deps.ts';
 import { load } from "./load.ts";
+import { ys2js } from "./convert.ts";
 
-let [mod] = Deno.args;
-if (!mod) {
+let [script] = Deno.args;
+if (!script) {
   console.error('ys SCRIPT');
 } else {
-  let url = isURL(mod) ? mod : `file://${resolve(mod)}`;
-  await run(() => load(url));
+  let url = isURL(script) ? script : `file://${resolve(script)}`;
+  await run(function*() {
+
+    // setup logging context to output to stdout.
+    yield* spawn(function*() {
+      let logs = yield* LogContext;
+      let i = yield* logs.output;
+      for (let next = yield* i; !next.done; next = yield* i) {
+        console.log(next.value.message)
+      }
+    });
+    // load the module
+    let mod = yield* load(url);
+
+    // if the module has a result, print it.
+    if (mod.value) {
+      console.log(ys2js(mod.value));
+    }
+  });
 }
 
 function isURL(value: string): boolean {
   try {
     new URL(value);
-    return true;
+    return true
   } catch (_) {
     return false;
   }
