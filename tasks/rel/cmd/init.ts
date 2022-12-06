d50299c78fd2bbc34b6cfbc1dbf07d6c4bc78fa4
Charles Lowell <cowboyd@frontside.com>
2022-12-06 17:31:39 -0600

    Explicitly pass all commands as an array to `sh()` on init
    There was a problem where a dirglob was not being interpreted properly
    because of the way we were splitting an string. See
    https://github.com/thefrontside/platformscript/actions/runs/3634349693/jobs/6132343127
    
    Instead This just passess each elemen as a separat item that way there
    is no ambiguity in the way the shell parses it.

--- tasks/rel/cmd/init.ts
+++ tasks/rel/cmd/init.ts
@@ -1,10 +1,9 @@
 import { sh } from "../exec.ts";
 
 try {
-  await sh("git config notes.rewriteRef refs/notes/*");
-  await sh("git config notes.displayRef refs/notes/*");
-  await sh("git config remote.origin.push +refs/notes/*:refs/notes/*");
-  await sh("git fetch origin refs/notes/*:refs/notes/*");
+  await sh(["git", "config", "notes.rewriteRef", "refs/notes/*"]);
+  await sh(["git", "config", "notes.displayRef", "refs/notes/*"]);
+  await sh(["git", "fetch", "-f", "origin", "refs/notes/*:refs/notes/*"]);
 } catch (error) {
   if (error.name === "ShellError") {
     console.error(error.message);
