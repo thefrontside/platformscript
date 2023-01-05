2ad858493b79072534aac1c8601d9ea69e40afb7
Charles Lowell <cowboyd@frontside.com>
2023-01-05 17:09:31 -0600

    Add index page which will contain explainer text

--- www/fresh.gen.ts
+++ www/fresh.gen.ts
@@ -6,9 +6,10 @@ import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/_404.tsx";
 import * as $2 from "./routes/_500.tsx";
-import * as $3 from "./routes/docs/[...slug].tsx";
-import * as $4 from "./routes/gfm.css.ts";
-import * as $5 from "./routes/index.tsx";
+import * as $3 from "./routes/_middleware.ts";
+import * as $4 from "./routes/docs/[...slug].tsx";
+import * as $5 from "./routes/gfm.css.ts";
+import * as $6 from "./routes/index.tsx";
 import * as $$0 from "./islands/CopyArea.tsx";
 
 const manifest = {
@@ -16,9 +17,10 @@ const manifest = {
     "./routes/[name].tsx": $0,
     "./routes/_404.tsx": $1,
     "./routes/_500.tsx": $2,
-    "./routes/docs/[...slug].tsx": $3,
-    "./routes/gfm.css.ts": $4,
-    "./routes/index.tsx": $5,
+    "./routes/_middleware.ts": $3,
+    "./routes/docs/[...slug].tsx": $4,
+    "./routes/gfm.css.ts": $5,
+    "./routes/index.tsx": $6,
   },
   islands: {
     "./islands/CopyArea.tsx": $$0,
--- www/routes/index.tsx
+++ www/routes/index.tsx
@@ -1,8 +1,48 @@
-import { Handlers } from "$fresh/server.ts";
+import type { FunctionComponent } from "preact";
+import type { Handlers, PageProps } from "$fresh/server.ts";
+import { Head } from "$fresh/runtime.ts";
 
-export const handler: Handlers = {
-  GET(req) {
-    let base = req.headers.get("x-base") ?? req.url;
-    return Response.redirect(new URL("./docs/introduction", base));
-  },
-};
+import Header from "../components/Header.tsx";
+import Footer from "../components/Footer.tsx";
+
+export const handler: Handlers<unknown, { base: string }> = {
+  async GET(_req, cxt) {
+    return await cxt.render({ base: cxt.state.base });
+  }
+}
+
+
+const description = "PlatformScript is a friendly, fully-featured programming language that uses 100% YAML for its syntax."
+
+export interface PageData {
+  base: string;
+}
+
+export function createPage<T extends PageData = PageData>(Component: FunctionComponent<PageProps<T>>) {
+  return function Page(props: PageProps<T>) {
+    return (
+      <>
+        <Head>
+          <base href={props.data.base} />
+        </Head>
+        <Header title="abount" active="/" />
+        <Component {...props}/>
+        <Footer/>
+      </>
+    );
+  }
+}
+
+
+export default createPage(function IndexPage(props: PageProps<PageData>) {
+  return (
+    <>
+      <Head>
+        <title>PlatformScript: A friendly, featureful programming language that uses 100% YAML for its syntax.</title>
+        <meta name="description" content={description} />
+      </Head>
+      <p>{description}</p>
+      <a href="/docs/introduction">get started</a>
+    </>
+  );
+});
