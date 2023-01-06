d18f4efbb23db2d18a080a380629ef49784abc8c
Charles Lowell <cowboyd@frontside.com>
2023-01-06 09:12:35 -0600

    Add feature sections to platformscript page

--- www/routes/index.tsx
+++ www/routes/index.tsx
@@ -1,4 +1,4 @@
-import type { FunctionComponent } from "preact";
+import type { FunctionComponent, ComponentChildren } from "preact";
 import type { Handlers, PageProps } from "$fresh/server.ts";
 import { Head } from "$fresh/runtime.ts";
 
@@ -11,8 +11,7 @@ export const handler: Handlers<unknown, { base: string }> = {
   }
 }
 
-
-const description = "PlatformScript is a friendly, fully-featured programming language with 100% pure YAML syntax."
+const description = "PlatformScript is a friendly, full featured programming language with pure YAML syntax."
 
 export interface PageData {
   base: string;
@@ -33,16 +32,38 @@ export function createPage<T extends PageData = PageData>(Component: FunctionCom
   }
 }
 
+function FeatureSection({ title, children }: { title: string, children: ComponentChildren }) {
+  return (
+    <section class="m-6 text-xl lg:text-3xl">
+      <h1 class="font-bold mb-4">{title}</h1>
+      <p>{children}</p>
+    </section>
+  )
+}
 
-export default createPage(function IndexPage(props: PageProps<PageData>) {
+export default createPage(function IndexPage() {
   return (
     <>
       <Head>
         <title>PlatformScript: A friendly, featureful programming language that uses 100% YAML for its syntax.</title>
         <meta name="description" content={description} />
       </Head>
-      <p>{description}</p>
-      <a href="/docs/introduction">get started</a>
+      <main class="p-6">
+        <div class="text-center pt-32 pb-32">
+          <h1 class="mb-12 p-6 font-bold  text-5xl lg:text-7xl">{description}</h1>
+          <a class="p-4 clear border-1 bg-blue-100 hover:bg-blue-300" href="/docs/introduction">get started</a>
+        </div>
+        <FeatureSection title="Use any YAML document as data, evaluate it or run it">
+            PlatformScript imports YAML from any URL. It evaluates the data, which can optionally start processes or execute tasks.
+        </FeatureSection>
+
+        <FeatureSection title="Share reusable YAML functions">
+          <p>Any PlatformScript YAML can be a module if it’s accessible via a URL. You can create reusable features by simply uploading the file to a gist, any HTTP server, or anywhere on the file system.</p>
+        </FeatureSection>
+        <FeatureSection title="Create executable applications">
+          <p>You can create a CLI tool by pointing the PlatformScript executable at a YAML document with CLI command configurations. PlatformScript will automatically download the YAML script, interpret it and run the commands.</p>
+        </FeatureSection>
+      </main>
     </>
   );
 });
