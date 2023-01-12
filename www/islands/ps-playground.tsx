207350eb141f748f67aaf562ff00674e630032e4
Charles Lowell <cowboyd@frontside.com>
2023-01-12 08:35:53 -0600

    parse YAML input as PSScript.

--- www/islands/ps-playground.tsx
+++ www/islands/ps-playground.tsx
@@ -1,7 +1,9 @@
 import { MonacoEditor } from "../components/monaco-editor.tsx";
 import type { MonacoEditorCreateOptions } from "../types/monaco.ts";
-import { useState } from "preact/hooks";
+import { useState, useEffect } from "preact/hooks";
 import { useTask, sleep } from "../hooks/use-task.ts";
+
+import { parse } from "../../mod.ts";
 
 //https://microsoft.github.io/monaco-editor/api/interfaces/monaco.editor.IStandaloneEditorConstructionOptions.html
 const sharedOptions: MonacoEditorCreateOptions = {
@@ -20,16 +22,23 @@ const previewOptions = {
 }
 
 export default function PSPlayground() {
+  let value = "true";
   let [result, setResult] = useState("");
   let onChange = useTask<string>(function* (value) {
     yield* sleep(200);
-    setResult(value)
+    try {
+      parse(value);
+      setResult(value)
+    } catch (error) {
+      setResult(`${error.name}: ${error.message}`);
+    }
   }, [result]);
+
+  useEffect(() => setResult(value), []);
 
   return (
     <div class="h-full grid grid-cols-2 gap-4">
-      <MonacoEditor value="true" language="yaml" options={sourceOptions} onChange={onChange} />
-
+      <MonacoEditor value={value} language="yaml" options={sourceOptions} onChange={onChange} />
       <MonacoEditor value={String(result)} language="yaml" options={previewOptions} />
     </div>
   )
