82edad0453f1b0766a6631842a1727732f6fd9c1
Charles Lowell <cowboyd@frontside.com>
2023-01-25 10:57:27 -0600

    print() tests

--- mod.ts
+++ mod.ts
@@ -2,6 +2,7 @@ export * from "./types.ts";
 export * from "./convert.ts";
 export * from "./read.ts";
 export * from "./evaluate.ts";
+export * from "./print.ts";
 export * from "./load.ts";
 export * from "./platformscript.ts";
 export * from "./data.ts";
