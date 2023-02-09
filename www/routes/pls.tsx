a19912993d288724b70362106d92f2052d354c01
Charles Lowell <cowboyd@frontside.com>
2023-02-09 09:04:27 -0600

    ✨redirect install route to specific version

--- www/routes/pls.tsx
+++ www/routes/pls.tsx
@@ -1,8 +1,10 @@
 import type { Handlers } from "$fresh/server.ts";
 
+import VERSION from "../../version.json" assert { type: "json" };
+
 export const handler: Handlers = {
   GET() {
-    const path = `https://deno.land/x/platformscript/cli/pls.ts`;
+    const path = `https://deno.land/x/platformscript@${VERSION}/cli/pls.ts`;
     return new Response(`Redirecting to ${path}`, {
       headers: { "Location": path },
       status: 307,
